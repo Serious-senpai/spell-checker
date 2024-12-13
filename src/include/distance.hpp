95e429ca3ba90b31a20ee9ceff453c31c2e0349f
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-12-13 20:10:02 +0700

    Fix Damerau-Levenshtein distance for UTF-8

--- src/include/distance.hpp
+++ src/include/distance.hpp
@@ -8,6 +8,8 @@ std::size_t _levenshtein_dp(
     const std::string &second,
     const std::size_t &i,
     const std::size_t &j,
+    const std::vector<std::size_t> &offset_i,
+    const std::vector<std::size_t> &offset_j,
     std::vector<std::vector<std::size_t>> &dp)
 {
     std::size_t &result = dp[i][j];
@@ -16,6 +18,7 @@ std::size_t _levenshtein_dp(
         return result;
     }
 
+    // std::cerr << std::make_pair(i, j) << " Comparing \"" << first.substr(0, offset_i[i]) << "\" and \"" << second.substr(0, offset_j[j]) << "\"" << std::endl;
     if (i == j && i == 0)
     {
         return result = 0;
@@ -23,23 +26,37 @@ std::size_t _levenshtein_dp(
 
     if (i > 0)
     {
-        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 1, j, dp) + 1);
+        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 1, j, offset_i, offset_j, dp) + 1);
     }
 
     if (j > 0)
     {
-        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i, j - 1, dp) + 1);
+        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i, j - 1, offset_i, offset_j, dp) + 1);
     }
 
     if (i > 0 && j > 0)
     {
-        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 1, j - 1, dp) + (first[i - 1] != second[j - 1]));
+        bool ij_equal = std::equal(
+            first.begin() + offset_i[i - 1], first.begin() + offset_i[i],
+            second.begin() + offset_j[j - 1], second.begin() + offset_j[j]);
+
+        result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 1, j - 1, offset_i, offset_j, dp) + !ij_equal);
 
         if constexpr (_AllowTransposition)
         {
-            if (i > 1 && j > 1 && first[i - 1] == second[j - 2] && first[i - 2] == second[j - 1])
+            if (i > 1 && j > 1)
             {
-                result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 2, j - 2, dp) + 1);
+                bool ix_j_equal = std::equal(
+                    first.begin() + offset_i[i - 2], first.begin() + offset_i[i - 1],
+                    second.begin() + offset_j[j - 1], second.begin() + offset_j[j]);
+                bool i_jx_equal = std::equal(
+                    first.begin() + offset_i[i - 1], first.begin() + offset_i[i],
+                    second.begin() + offset_j[j - 2], second.begin() + offset_j[j - 1]);
+
+                if (ix_j_equal && i_jx_equal)
+                {
+                    result = std::min(result, _levenshtein_dp<_AllowTransposition>(first, second, i - 2, j - 2, offset_i, offset_j, dp) + 1);
+                }
             }
         }
     }
@@ -51,5 +68,26 @@ std::size_t damerau_levenshtein(const std::string &first, const std::string &sec
 {
     std::size_t n = first.size(), m = second.size();
     std::vector<std::vector<std::size_t>> dp(n + 1, std::vector<std::size_t>(m + 1, std::numeric_limits<std::size_t>::max()));
-    return _levenshtein_dp<true>(first, second, n, m, dp);
+
+    std::vector<std::size_t> offset_i;
+    for (std::size_t i = 0; i < n; i++)
+    {
+        if ((first[i] & 0xC0) != 0x80)
+        {
+            offset_i.push_back(i);
+        }
+    }
+    offset_i.push_back(n);
+
+    std::vector<std::size_t> offset_j;
+    for (std::size_t j = 0; j < m; j++)
+    {
+        if ((second[j] & 0xC0) != 0x80)
+        {
+            offset_j.push_back(j);
+        }
+    }
+    offset_j.push_back(m);
+
+    return _levenshtein_dp<true>(first, second, offset_i.size() - 1, offset_j.size() - 1, offset_i, offset_j, dp);
 }
