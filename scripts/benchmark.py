937a7df5f34efc2b4c364192ca39925447666bd5
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-12-25 17:24:46 +0700

    Fix benchmarking script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,18 +1,26 @@
 from __future__ import annotations
 
 import json
+from typing import Iterable
 
 from config import Data, ROOT
+
+
+def benchmark_token_generator(data: Data) -> Iterable[str]:
+    for annotation in data["annotations"]:
+        if annotation["current_syllable"] == "":
+            continue
+
+        yield annotation["current_syllable"] if len(annotation["alternative_syllables"]) == 0 else annotation["alternative_syllables"][0]
 
 
 matched = total = 0
 with open(ROOT / "extern" / "VSEC" / "Dataset" / "VSEC.jsonl", "r", encoding="utf-8") as benchmark:
     with open(ROOT / "data" / "output.txt", "r", encoding="utf-8") as reader:
-        for benchmark_line, reader_lines in zip(benchmark, reader, strict=True):
+        for benchmark_line, reader_line in zip(benchmark, reader, strict=True):
             benchmark_data: Data = json.loads(benchmark_line)
 
-            for annotation, token in zip(benchmark_data["annotations"], reader_lines.split(), strict=True):
-                correct = annotation["current_syllable"] if len(annotation["alternative_syllables"]) == 0 else annotation["alternative_syllables"][0]
+            for correct, token in zip(benchmark_token_generator(benchmark_data), reader_line.split(), strict=True):
                 total += 1
                 matched += correct == token
 
